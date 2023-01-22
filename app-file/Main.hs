63771b45a6bfeeb3a99e05363a444e27b7e35339
Anteproperispomenon <30732161+Anteproperispomenon@users.noreply.github.com>
2023-01-22 18:11:19 -0500

    Now shows text preview.

--- app-file/Main.hs
+++ app-file/Main.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Control.Lens
 import Data.Maybe
 import Data.Text (Text)
+import Data.Text qualified as T
 import Monomer
 import TextShow
 
@@ -31,8 +33,8 @@ data AppModel = AppModel
   , _outputOrth :: OutputOrth
   , _inputFile :: Text
   , _outputFile :: Text
-  -- , _inputText :: Text
-  -- , _outputText :: Text
+  , _inputText :: Text
+  , _outputText :: Text
   } deriving (Eq, Show)
 
 data AppEvent
@@ -43,6 +45,7 @@ data AppEvent
   | AppOpenFile -- triggers dialog
   | AppSaveFile -- triggers dialog
   | AppWriteFile -- Actually writes content to file
+  | AppGotInput Text
   | AppNull -- Empty event
   deriving (Eq, Show)
 
@@ -98,13 +101,13 @@ buildUI wenv model = widgetTree where
     , spacer
     , button "Select File" AppOpenFile
     , dropTarget (\txt -> AppSetInput txt) (textField_ inputFile [readOnly])
-    -- , spacer
-    -- , (textArea_ inputText [readOnly])
+    , spacer
+    , vscroll $ (textArea_ inputText [readOnly]) `styleBasic` [textFont "Universal"]
     , spacer
     , button "Choose Destination" AppSaveFile
     , (textField_ outputFile [readOnly])
-    -- spacer
-    -- , (textArea_ outputText [])
+    , spacer
+    , vscroll $ (textArea_ outputText [readOnly]) `styleBasic` [textFont "Universal"]
     , spacer
     , button "Save File" AppWriteFile
     ] `styleBasic` [padding 10]
@@ -119,10 +122,11 @@ handleEvent wenv node model evt = case evt of
   AppInit -> []
   AppNull -> []
   AppIncrease -> [Model (model & clickCount +~ 1)]
-  (AppSetInput  fnm) -> [Model (model & inputFile  .~ fnm)]
+  (AppSetInput  fnm) -> [Model (model & inputFile  .~ fnm), Task $ AppGotInput <$> TU.readFile (T.unpack fnm)]
   (AppSetOutput fnm) -> [Model (model & outputFile .~ fnm)]
   AppOpenFile -> [Task $ handleFile1 <$> openFileDialog "Open Input File" "%HOME%" [] "Text Files" False]
   AppSaveFile -> [Task $ handleFile2 <$> saveFileDialog "Select Output File" "%HOME%" [] "Text Files"]
+  AppGotInput txt -> [Model (model & inputText .~ txt & outputText .~ getConversion txt)]
   AppWriteFile -> [] -- for now
   where 
     handleFile1 :: Maybe [Text] -> AppEvent
@@ -132,6 +136,11 @@ handleEvent wenv node model evt = case evt of
     handleFile2 :: Maybe Text -> AppEvent
     handleFile2 Nothing = AppNull
     handleFile2 (Just fnm) = AppSetOutput fnm
+    getConversion :: Text -> Text
+    getConversion inpTxt = let 
+      inpO = model ^. inputOrth
+      outO = model ^. outputOrth
+      in decodeKwakwalaD outO $ parseKwakwalaD inpO inpTxt
 
 
 -- KurintoSansAux-Rg.ttf
@@ -150,4 +159,4 @@ main = do
       appFontDef "Universal" "./assets/fonts/KurintoSans-Rg.ttf",
       appInitEvent AppInit
       ]
-    model = AppModel 0 IUmista OUmista "" ""
+    model = AppModel 0 IUmista OUmista "" "" "" ""

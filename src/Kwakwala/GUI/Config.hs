ae372b48db408563ee4bcf244f91cef445cfd3d5
David Wilson <30732161+Anteproperispomenon@users.noreply.github.com>
2023-02-09 15:44:53 -0500

    Created basic config type.
    Along with a widget that'll modify config settings.

---
+++ src/Kwakwala/GUI/Config.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Kwakwala.GUI.Config
+  ( KwakConfigModel(..)
+  , kcmGrubbUseJ
+  ) where
+
+import Control.Lens
+import Control.Lens.Lens (ALens')
+import Data.Default
+import Data.Text (Text)
+import Data.Text qualified as T
+
+import Monomer
+
+-- | A model for the types of config
+-- that are used.
+data KwakConfigModel = KwakConfigModel
+  { _kcmGrubbUseJ :: Bool
+  } deriving (Eq, Show)
+
+instance Default KwakConfigModel where
+  def = KwakConfigModel
+    { _kcmGrubbUseJ = True
+    }
+
+data KwakConfigEvent
+  = KwakEventTemp
+  deriving (Show, Eq)
+
+makeLenses 'KwakConfigModel
+
+-- | Create a node for a config widget
+kwakConfigWidget :: WidgetEvent e => ALens' s KwakConfigModel -> (KwakConfigEvent -> e) -> WidgetNode s e
+kwakConfigWidget mdlLens f = vscroll $ vstack
+  [ labeledCheckbox "Use 'J' to represent the phoneme /h/ in Grubb" ((cloneLens mdlLens) . kcmGrubbUseJ)
+  ]
+
+
+
+
